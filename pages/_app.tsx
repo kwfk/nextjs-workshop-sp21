d47caf9180dce4726265d91b0db34c3af4c4e6d8
Frederick Kim <freddy.kim8@gmail.com>
2021-03-05 02:49:00 -0800

    add material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { useEffect } from 'react';
+import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { StylesProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../utils/theme';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Remove the server-side injected CSS
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StylesProvider>
+    </>
+  );
 };
 
 export default MyApp;
---
+++ utils/theme.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      light: '#F8F4FF',
+      main: '#9056EF',
+      dark: '#71579C',
+    },
+    secondary: {
+      light: '#EB6658',
+      main: '#EB6658',
+      dark: '#EB6658',
+    },
+  },
+});
